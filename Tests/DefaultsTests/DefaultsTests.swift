4342932b92c084e74127a4918a4a5f070d6fd02d
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2018-04-15 09:42:31 +0200

    Use native data coding

--- Tests/DefaultsTests/DefaultsTests.swift
+++ Tests/DefaultsTests/DefaultsTests.swift
@@ -13,6 +13,7 @@ extension Defaults.Keys {
 	static let key = Defaults.Key<Bool>("key", default: false)
 	static let url = Defaults.Key<URL>("url", default: fixtureUrl)
 	static let `enum` = Defaults.Key<FixtureEnum>("enum", default: .oneHour)
+	static let data = Defaults.Key<Data>("data", default: Data(bytes: []))
 }
 
 final class DefaultsTests: XCTestCase {
@@ -51,6 +52,14 @@ final class DefaultsTests: XCTestCase {
 
 	func testEnumType() {
 		XCTAssertEqual(defaults[.enum], FixtureEnum.oneHour)
+	}
+
+	func testDataType() {
+		XCTAssertEqual(defaults[.data], Data(bytes: []))
+
+		let newData = Data(bytes: [0xFF])
+		defaults[.data] = newData
+		XCTAssertEqual(defaults[.data], newData)
 	}
 
 	func testClear() {
