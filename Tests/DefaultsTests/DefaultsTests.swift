7bf767ab20c64e78028a01951ca1d5014945f98e
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-12 16:51:37 +0700

    Try something

--- Tests/DefaultsTests/DefaultsTests.swift
+++ Tests/DefaultsTests/DefaultsTests.swift
@@ -112,17 +112,14 @@ final class DefaultsTests: XCTestCase {
 
 	func testObserveKey() {
 		let key = Defaults.Key<Bool>("observeKey", default: false)
-
-		// Using `weak` here to prevent `.fullfill()` to be called multiple times, since the callback is called multiple times on Travis CI for some reason. Not reproducible locally.
-		weak var expect = expectation(description: "Observation closure being called")
+		let expect = expectation(description: "Observation closure being called")
 
 		var observation: DefaultsObservation!
 		observation = defaults.observe(key, options: [.old, .new]) { change in
 			XCTAssertFalse(change.oldValue)
 			XCTAssertTrue(change.newValue)
-			expect?.fulfill()
-			expect = nil
 			observation.invalidate()
+			expect.fulfill()
 		}
 
 		defaults[key] = true
@@ -132,15 +129,14 @@ final class DefaultsTests: XCTestCase {
 
 	func testObserveOptionalKey() {
 		let key = Defaults.OptionalKey<Bool>("observeOptionalKey")
-		weak var expect = expectation(description: "Observation closure being called")
+		let expect = expectation(description: "Observation closure being called")
 
 		var observation: DefaultsObservation!
 		observation = defaults.observe(key, options: [.old, .new]) { change in
 			XCTAssertNil(change.oldValue)
 			XCTAssertTrue(change.newValue!)
-			expect?.fulfill()
-			expect = nil
 			observation.invalidate()
+			expect.fulfill()
 		}
 
 		defaults[key] = true
@@ -152,15 +148,14 @@ final class DefaultsTests: XCTestCase {
 		let fixtureURL = URL(string: "https://sindresorhus.com")!
 		let fixtureURL2 = URL(string: "https://example.com")!
 		let key = Defaults.Key<URL>("observeKeyURL", default: fixtureURL)
-		weak var expect = expectation(description: "Observation closure being called")
+		let expect = expectation(description: "Observation closure being called")
 
 		var observation: DefaultsObservation!
 		observation = defaults.observe(key, options: [.old, .new]) { change in
 			XCTAssertEqual(change.oldValue, fixtureURL)
 			XCTAssertEqual(change.newValue, fixtureURL2)
-			expect?.fulfill()
-			expect = nil
 			observation.invalidate()
+			expect.fulfill()
 		}
 
 		defaults[key] = fixtureURL2
@@ -170,15 +165,14 @@ final class DefaultsTests: XCTestCase {
 
 	func testObserveKeyEnum() {
 		let key = Defaults.Key<FixtureEnum>("observeKeyEnum", default: .oneHour)
-		weak var expect = expectation(description: "Observation closure being called")
+		let expect = expectation(description: "Observation closure being called")
 
 		var observation: DefaultsObservation!
 		observation = defaults.observe(key, options: [.old, .new]) { change in
 			XCTAssertEqual(change.oldValue, .oneHour)
 			XCTAssertEqual(change.newValue, .tenMinutes)
-			expect?.fulfill()
-			expect = nil
 			observation.invalidate()
+			expect.fulfill()
 		}
 
 		defaults[key] = .tenMinutes
