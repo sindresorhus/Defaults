745a96e148950a6fbaddcaa17db40b53ded9b954
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-20 20:17:46 +0700

    Update Observation+Combine.swift

--- Sources/Defaults/Observation+Combine.swift
+++ Sources/Defaults/Observation+Combine.swift
@@ -1,150 +1,136 @@
-//
-//  Observation+Combine.swift
-//  Defaults-macOS
-//
-//  Created by Kacper Raczy on 29/12/2019.
-//  Copyright © 2019 Defaults. All rights reserved.
-//
-
-#if canImport(Combine)
-
 import Foundation
 import Combine
 
 extension Defaults {
 	/**
-		Custom Subscription for user defaults key observation
+	Custom `Subscription` for `UserDefaults` key observation.
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	final class DefaultsSubscription<SubscriberType: Subscriber>: Subscription where SubscriberType.Input == BaseChange {
 		private var subscriber: SubscriberType?
 		private var observation: UserDefaultsKeyObservation?
-		
+
 		init(subscriber: SubscriberType, suite: UserDefaults, key: String, options: NSKeyValueObservingOptions) {
 			self.subscriber = subscriber
-			self.observation = UserDefaultsKeyObservation(object: suite, key: key, callback: observationCallback(_:))
+			self.observation = UserDefaultsKeyObservation(
+				object: suite,
+				key: key,
+				callback: observationCallback(_:)
+			)
 			self.observation?.start(options: options)
 		}
-		
+
 		func request(_ demand: Subscribers.Demand) {
-			// nothing as we send events only when they occur
+			// Nothing as we send events only when they occur.
 		}
-		
+
 		func cancel() {
 			observation?.invalidate()
 			observation = nil
 			subscriber = nil
 		}
-		
+
 		private func observationCallback(_ change: BaseChange) {
 			_ = subscriber?.receive(change)
 		}
 	}
 	
 	/**
-		Custom Publisher, which is using DefaultsSubscription
+	Custom Publisher, which is using DefaultsSubscription.
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	struct DefaultsPublisher: Publisher {
 		typealias Output = BaseChange
 		typealias Failure = Never
-		
+
 		private let suite: UserDefaults
 		private let key: String
 		private let options: NSKeyValueObservingOptions
-		
+
 		init(suite: UserDefaults, key: String, options: NSKeyValueObservingOptions) {
 			self.suite = suite
 			self.key = key
 			self.options = options
 		}
-		
+
 		func receive<S>(subscriber: S) where S : Subscriber, DefaultsPublisher.Failure == S.Failure, DefaultsPublisher.Output == S.Input {
-			let subscription = DefaultsSubscription(subscriber: subscriber,
-													suite: suite,
-													key: key,
-													options: options)
+			let subscription = DefaultsSubscription(
+				subscriber: subscriber,
+				suite: suite,
+				key: key,
+				options: options
+			)
+
 			subscriber.receive(subscription: subscription)
 		}
 	}
 
 	/**
-		Returns type-erased Publisher object, publishing changes related to specified key.
+	Returns a type-erased `Publisher` that publishes changes related to the given key.
 
-		```
-		extension Defaults.Keys {
-			static let isUnicornMode = Key<Bool>("isUnicornMode", default: false)
-		}
+	```
+	extension Defaults.Keys {
+		static let isUnicornMode = Key<Bool>("isUnicornMode", default: false)
+	}
 
-		let publisher = Defaults.publisher(.isUnicornMode).map { $0.newValue }
-		let cancellable = publisher.sink { (value)
-			print(value)
-			//=> false
-		}
-		```
+	let publisher = Defaults.publisher(.isUnicornMode).map { $0.newValue }
+
+	let cancellable = publisher.sink { value in
+		print(value)
+		//=> false
+	}
+	```
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	public static func publisher<T: Codable>(
 		_ key: Defaults.Key<T>,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<KeyChange<T>, Never> {
-		let publisher =
-			DefaultsPublisher(suite: key.suite, key: key.name, options: options)
-				.map({
-					return KeyChange<T>(change: $0, defaultValue: key.defaultValue)
-				})
-		
+		let publisher = DefaultsPublisher(suite: key.suite, key: key.name, options: options)
+			.map { KeyChange<T>(change: $0, defaultValue: key.defaultValue) }
+
 		return AnyPublisher(publisher)
 	}
 
 	/**
-		Returns type-erased Publisher object, publishing changes related to specified key.
+	Returns a type-erased `Publisher` that publishes changes related to the given key.
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	public static func publisher<T: NSSecureCoding>(
 		_ key: Defaults.NSSecureCodingKey<T>,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<NSSecureCodingKeyChange<T>, Never> {
-		let publisher =
-			DefaultsPublisher(suite: key.suite, key: key.name, options: options)
-				.map({
-					return NSSecureCodingKeyChange<T>(change: $0, defaultValue: key.defaultValue)
-				})
-		
+		let publisher = DefaultsPublisher(suite: key.suite, key: key.name, options: options)
+			.map { NSSecureCodingKeyChange<T>(change: $0, defaultValue: key.defaultValue) }
+
 		return AnyPublisher(publisher)
 	}
 
 	/**
-		Returns type-erased Publisher object, publishing changes related to specified optional key.
+	Returns a type-erased `Publisher` that publishes changes related to the given optional key.
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	public static func publisher<T: Codable>(
 		_ key: Defaults.OptionalKey<T>,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<OptionalKeyChange<T>, Never> {
-		let publisher =
-			DefaultsPublisher(suite: key.suite, key: key.name, options: options)
-				.map({
-					return OptionalKeyChange<T>(change: $0)
-				})
-		
+		let publisher = DefaultsPublisher(suite: key.suite, key: key.name, options: options)
+			.map { OptionalKeyChange<T>(change: $0) }
+
 		return AnyPublisher(publisher)
 	}
 	
 	/**
-		Returns type-erased Publisher object, publishing changes related to specified optional key.
+	Returns a type-erased `Publisher` that publishes changes related to the given optional key.
 	*/
 	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, iOSApplicationExtension 13.0, macOSApplicationExtension 10.15, tvOSApplicationExtension 13.0, watchOSApplicationExtension 6.0, *)
 	public static func publisher<T: NSSecureCoding>(
 		_ key: Defaults.NSSecureCodingOptionalKey<T>,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<NSSecureCodingOptionalKeyChange<T>, Never> {
-		let publisher =
-			DefaultsPublisher(suite: key.suite, key: key.name, options: options)
-				.map({
-					return NSSecureCodingOptionalKeyChange<T>(change: $0)
-				})
-		
+		let publisher = DefaultsPublisher(suite: key.suite, key: key.name, options: options)
+			.map { NSSecureCodingOptionalKeyChange<T>(change: $0) }
+
 		return AnyPublisher(publisher)
 	}
 
@@ -161,9 +147,7 @@ extension Defaults {
 		keys: Defaults.Key<T>...,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<Void, Never> {
-		let initial =
-			Empty<Void, Never>(completeImmediately: false)
-				.eraseToAnyPublisher()
+		let initial = Empty<Void, Never>(completeImmediately: false).eraseToAnyPublisher()
 
 		let combinedPublisher =
 			keys.map { key in
@@ -173,7 +157,7 @@ extension Defaults {
 			}.reduce(initial) { (combined, keyPublisher) in
 				combined.merge(with: keyPublisher).eraseToAnyPublisher()
 			}
-		
+
 		return combinedPublisher
 	}
 
@@ -182,9 +166,7 @@ extension Defaults {
 		keys: Defaults.OptionalKey<T>...,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<Void, Never> {
-		let initial =
-			Empty<Void, Never>(completeImmediately: false)
-				.eraseToAnyPublisher()
+		let initial = Empty<Void, Never>(completeImmediately: false).eraseToAnyPublisher()
 
 		let combinedPublisher =
 			keys.map { key in
@@ -194,7 +176,7 @@ extension Defaults {
 			}.reduce(initial) { (combined, keyPublisher) in
 				combined.merge(with: keyPublisher).eraseToAnyPublisher()
 			}
-		
+
 		return combinedPublisher
 	}
 
@@ -203,9 +185,7 @@ extension Defaults {
 		keys: Defaults.NSSecureCodingKey<T>...,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<Void, Never> {
-		let initial =
-			Empty<Void, Never>(completeImmediately: false)
-				.eraseToAnyPublisher()
+		let initial = Empty<Void, Never>(completeImmediately: false).eraseToAnyPublisher()
 
 		let combinedPublisher =
 			keys.map { key in
@@ -215,7 +195,7 @@ extension Defaults {
 			}.reduce(initial) { (combined, keyPublisher) in
 				combined.merge(with: keyPublisher).eraseToAnyPublisher()
 			}
-		
+
 		return combinedPublisher
 	}
 
@@ -224,9 +204,7 @@ extension Defaults {
 		keys: Defaults.NSSecureCodingOptionalKey<T>...,
 		options: NSKeyValueObservingOptions = [.initial, .old, .new]
 	) -> AnyPublisher<Void, Never> {
-		let initial =
-			Empty<Void, Never>(completeImmediately: false)
-				.eraseToAnyPublisher()
+		let initial = Empty<Void, Never>(completeImmediately: false).eraseToAnyPublisher()
 
 		let combinedPublisher =
 			keys.map { key in
@@ -236,9 +214,7 @@ extension Defaults {
 			}.reduce(initial) { (combined, keyPublisher) in
 				combined.merge(with: keyPublisher).eraseToAnyPublisher()
 			}
-		
+
 		return combinedPublisher
 	}
 }
-
-#endif
