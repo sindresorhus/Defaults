d328da99d516a8075500786cf435d4c549a9cf8d
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-27 13:01:31 +0200

    Minor tweaks

--- Sources/Defaults/UserDefaults.swift
+++ Sources/Defaults/UserDefaults.swift
@@ -140,6 +140,7 @@ extension UserDefaults {
 	- Note: This only removes user-defined entries. System-defined entries will remain.
 	*/
 	public func removeAll() {
+		// We're not using `.removePersistentDomain(forName:)` as it requires knowing the suite name and also because it doesn't emit change events for each key, but rather just `UserDefaults.didChangeNotification`, which we don't subscribe to.
 		for key in dictionaryRepresentation().keys {
 			removeObject(forKey: key)
 		}
