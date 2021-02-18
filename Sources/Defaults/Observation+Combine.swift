fd45b1d2220f14d662e092185154309d9b404eeb
Iurii Iaremenko <iurii.iaremenko@gmail.com>
2021-02-18 11:05:23 +0200

    Update Equatable conformance for all key change types

--- Sources/Defaults/Observation+Combine.swift
+++ Sources/Defaults/Observation+Combine.swift
@@ -151,24 +151,4 @@ extension Defaults {
 		return combinedPublisher
 	}
 }
-
-@available(macOS 10.13, macOSApplicationExtension 10.13, watchOSApplicationExtension 4.0, *)
-extension Defaults.NSSecureCodingKeyChange: Equatable where Value: Equatable {
-	public static func == (lhs: Self, rhs: Self) -> Bool {
-		lhs.newValue == rhs.newValue
-	}
-}
-
-@available(macOS 10.13, macOSApplicationExtension 10.13, watchOSApplicationExtension 4.0, *)
-extension Defaults.NSSecureCodingOptionalKeyChange: Equatable where Value: Equatable {
-	public static func == (lhs: Self, rhs: Self) -> Bool {
-		lhs.newValue == rhs.newValue
-	}
-}
-
-extension Defaults.KeyChange: Equatable where Value: Equatable {
-	public static func == (lhs: Self, rhs: Self) -> Bool {
-		lhs.newValue == rhs.newValue
-	}
-}
 #endif
