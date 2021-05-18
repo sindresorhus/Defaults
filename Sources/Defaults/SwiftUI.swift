63d93f97ad545c8bceb125a8a36175ea705f7cf5
Sindre Sorhus <sindresorhus@gmail.com>
2021-05-18 19:56:55 +0700

    Add `Defaults.Toggle` (#69)

--- Sources/Defaults/SwiftUI.swift
+++ Sources/Defaults/SwiftUI.swift
@@ -5,9 +5,10 @@ import Combine
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension Defaults {
 	final class Observable<Value: Serializable>: ObservableObject {
-		let objectWillChange = ObservableObjectPublisher()
 		private var observation: DefaultsObservation?
 		private let key: Defaults.Key<Value>
+
+		let objectWillChange = ObservableObjectPublisher()
 
 		var value: Value {
 			get { Defaults[key] }
@@ -44,6 +45,8 @@ public struct Default<Value: Defaults.Serializable>: DynamicProperty {
 	public typealias Publisher = AnyPublisher<Defaults.KeyChange<Value>, Never>
 
 	private let key: Defaults.Key<Value>
+
+	// Intentionally using `@ObservedObjected` over `@StateObject` so that the key can be dynamicaly changed.
 	@ObservedObject private var observable: Defaults.Observable<Value>
 
 	/**
@@ -61,7 +64,10 @@ public struct Default<Value: Defaults.Serializable>: DynamicProperty {
 
 		var body: some View {
 			Text("Has Unicorn: \(hasUnicorn)")
-			Toggle("Toggle Unicorn", isOn: $hasUnicorn)
+			Toggle("Toggle", isOn: $hasUnicorn)
+			Button("Reset") {
+				_hasUnicorn.reset()
+			}
 		}
 	}
 	```
@@ -108,6 +114,103 @@ public struct Default<Value: Defaults.Serializable>: DynamicProperty {
 	*/
 	public func reset() {
 		key.reset()
+	}
+}
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+extension Defaults {
+	/**
+	Creates a SwiftUI `Toggle` view that is connected to a `Defaults` key with a `Bool` value.
+
+	The toggle works exactly like the SwiftUI `Toggle`.
+
+	```
+	extension Defaults.Keys {
+		static let showAllDayEvents = Key<Bool>("showAllDayEvents", default: false)
+	}
+
+	struct ShowAllDayEventsSetting: View {
+		var body: some View {
+			Defaults.Toggle("Show All-Day Events", key: .showAllDayEvents)
+		}
+	}
+	```
+
+	You can also listen to changes:
+
+	```
+	struct ShowAllDayEventsSetting: View {
+		var body: some View {
+			Defaults.Toggle("Show All-Day Events", key: .showAllDayEvents)
+				// Note that this has to be directly attached to `Defaults.Toggle`. It's not `View#onChange()`.
+				.onChange {
+					print("Value", $0)
+				}
+		}
+	}
+	```
+	*/
+	public struct Toggle<Label, Key>: View where Label: View, Key: Defaults.Key<Bool> {
+		@ViewStorage private var onChange: ((Bool) -> Void)?
+
+		private let label: () -> Label
+
+		// Intentionally using `@ObservedObjected` over `@StateObject` so that the key can be dynamicaly changed.
+		@ObservedObject private var observable: Defaults.Observable<Bool>
+
+		public init(key: Key, @ViewBuilder label: @escaping () -> Label) {
+			self.label = label
+			self.observable = Defaults.Observable(key)
+		}
+
+		public var body: some View {
+			SwiftUI.Toggle(isOn: $observable.value, label: label)
+				.onChange(of: observable.value) {
+					onChange?($0)
+				}
+		}
+	}
+}
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+extension Defaults.Toggle where Label == Text {
+	public init<S>(_ title: S, key: Defaults.Key<Bool>) where S: StringProtocol {
+		self.label = { Text(title) }
+		self.observable = Defaults.Observable(key)
+	}
+}
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+extension Defaults.Toggle {
+	/// Do something when the value changes to a different value.
+	public func onChange(_ action: @escaping (Bool) -> Void) -> Self {
+		onChange = action
+		return self
+	}
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+@propertyWrapper
+private struct ViewStorage<Value>: DynamicProperty {
+	private final class ValueBox {
+		var value: Value
+
+		init(_ value: Value) {
+			self.value = value
+		}
+	}
+
+	@State private var valueBox: ValueBox
+
+	var wrappedValue: Value {
+		get { valueBox.value }
+		nonmutating set {
+			valueBox.value = newValue
+		}
+	}
+
+	init(wrappedValue value: @autoclosure @escaping () -> Value) {
+		self._valueBox = .init(wrappedValue: ValueBox(value()))
 	}
 }
 #endif
