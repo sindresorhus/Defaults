e072103009ed0f01176409ad19ed4283430a5058
Sindre Sorhus <sindresorhus@gmail.com>
2022-10-27 16:52:38 +0700

    Update Utilities.swift

--- Sources/Defaults/Utilities.swift
+++ Sources/Defaults/Utilities.swift
@@ -188,8 +188,9 @@ extension Defaults.Serializable {
 		{
 			return anyObject
 		}
+
 		guard let nextType = T.Serializable.self as? any Defaults.Serializable.Type else {
-			// This is a special case for the types which do not conform to `Defaults.Serializable`(ex. `Any`).
+			// This is a special case for the types which do not conform to `Defaults.Serializable` (for example, `Any`).
 			return T.bridge.deserialize(anyObject as? T.Serializable) as? T
 		}
 
