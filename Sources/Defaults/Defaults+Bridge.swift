d8a9f5105607c85b544558e7f5b51d6c360ba88b
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-04 17:58:57 +0700

    Remove value wrapper from CodableBridge (#151)
    Fixes #32

--- Sources/Defaults/Defaults+Bridge.swift
+++ Sources/Defaults/Defaults+Bridge.swift
@@ -12,13 +12,10 @@ extension Defaults.CodableBridge {
 		}
 
 		do {
-			// Some codable values like URL and enum are encoded as a top-level
-			// string which JSON can't handle, so we need to wrap it in an array
-			// We need this: https://forums.swift.org/t/allowing-top-level-fragments-in-jsondecoder/11750
 			let jsonEncoder = JSONEncoder()
 			jsonEncoder.outputFormatting = .sortedKeys
-			let data = try jsonEncoder.encode([value])
-			return String(String(data: data, encoding: .utf8)!.dropFirst().dropLast())
+			let data = try jsonEncoder.encode(value)
+			return String(data: data, encoding: .utf8)
 		} catch {
 			print(error)
 			return nil
@@ -30,7 +27,7 @@ extension Defaults.CodableBridge {
 			return nil
 		}
 
-		return [Value].init(jsonString: "[\(object)]")?.first
+		return Value(jsonString: object)
 	}
 }
 
