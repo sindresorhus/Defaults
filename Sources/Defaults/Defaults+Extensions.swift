61006c510086fbe9950beb25bde4f6396ebc8ae4
Sindre Sorhus <sindresorhus@gmail.com>
2024-04-20 15:13:31 +0700

    Add support for serializing `Color.Resolved`
    Fixes #157

--- Sources/Defaults/Defaults+Extensions.swift
+++ Sources/Defaults/Defaults+Extensions.swift
@@ -145,6 +145,11 @@ extension Color: Defaults.Serializable {
 	public static let bridge = Defaults.ColorBridge()
 }
 
+@available(macOS 14.0, iOS 17.0, tvOS 17.0, watchOS 9.0, visionOS 1.0, *)
+extension Color.Resolved: Defaults.Serializable {
+	public static let bridge = Defaults.ColorResolvedBridge()
+}
+
 extension Range: Defaults.RangeSerializable where Bound: Defaults.Serializable {
 	public static var bridge: Defaults.RangeBridge<Range> { Defaults.RangeBridge() }
 }
