91d4258a4f912dc2ae17b28a90cc1917ab6f14ca
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2024-09-18 12:39:22 -0400

    Add docstring to ObservableDefaults macro

--- Sources/DefaultsMacros/ObservableDefaults.swift
+++ Sources/DefaultsMacros/ObservableDefaults.swift
@@ -1,7 +1,43 @@
+import Defaults
 import Foundation
 
-import Defaults
+/**
+Attached macro that adds support for using ``Defaults`` in ``@Observable`` classes. **Important**: to
+prevent issues with ``@Observable``, you'll need to also add ``@ObservationIgnored`` to the attached
+property.
 
+This macro adds accessor blocks to the attached property similar to those added by `@Observable`.
+For example, given the following source:
+
+```swift
+@Observable
+final class CatModel {
+	@ObservableDefaults(Defaults.Keys.cat)
+	@ObservationIgnored
+	private var catName: String
+}
+```
+
+The macro will generate the following expansion:
+
+```swift
+@Observable
+final class CatModel {
+	@ObservationIgnored
+	private var catName: String {
+		get {
+			access(keypath: \.catName)
+			return Defaults[Defaults.Keys.cat]
+		}
+		set {
+			withMutation(keyPath: \catName) {
+				Defaults[Defaults.Keys.cat] = newValue
+			}
+		}
+	}
+}
+```
+*/
 @attached(accessor, names: named(get), named(set))
 public macro ObservableDefaults<Value>(_ key: Defaults.Key<Value>) =
 	#externalMacro(
